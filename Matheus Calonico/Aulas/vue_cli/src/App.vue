e3703bbb77041fccebd13d4e9c8d22551803abaf
Matheus dos Santos Calonico <matheus.calonico@dinfor.uniceub>
2026-03-25 21:41:39 -0300

    Aula vue.cli

---
+++ Matheus Calonico/Aulas/vue_cli/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HomeComponente />
+  <hr>
+ <HomeComponente />
+</div>
+</template>
+
+<script>
+
+import HomeComponente from './components/HomeComponente.vue';
+
+export default {
+  name: 'App',
+  components: { 
+    HomeComponente,
+  },
+  data() {
+    return {
+      
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
