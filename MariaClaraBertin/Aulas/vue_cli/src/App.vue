0b3beb075b117b6db8d58bdde6f52d6aaa55d1ce
Maria Clara Bertin Vieira Ferreira <bertinmaria@dinfor.uniceub>
2026-04-01 21:37:50 -0300

    novos componentes

--- MariaClaraBertin/Aulas/vue_cli/src/App.vue
+++ MariaClaraBertin/Aulas/vue_cli/src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <home-component />
-    <hr>
     <HomeComponent />
+    <hr>
+    <UsuarioComponent />
+    <hr>
+    <EstiloComponent />
   </div>
 </template>
 
 <script>
 
+import EstiloComponent from './components/EstiloComponent.vue';
 import HomeComponent from './components/HomeComponent.vue';
+import UsuarioComponent from './components/UsuarioComponent.vue';
 
 export default {
   name: 'App',
   components: {
     HomeComponent,
-  }
+    UsuarioComponent,
+    EstiloComponent,
+  },
+
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
 }
+
 </script>
 
 <style>
