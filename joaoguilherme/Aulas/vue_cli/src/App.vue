68f0d57ea9fe461cc6c205c59c5414dd82ec3ae2
Joao Guilherme Sousa Figueiredo <joaoguilhermesf@dinfor.uniceub>
2026-04-15 21:22:20 -0300

    Atualizando Vue 15/04

--- joaoguilherme/Aulas/vue_cli/src/App.vue
+++ joaoguilherme/Aulas/vue_cli/src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <home-component />
-  <hr />
-  <HomeComponent />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Meu Primeiro App Vue</h1>
+    <p class="subtitulo">Vamos aprender muito!</p>
+    <br />
+    <estilo-component />
+    <br />
+    <home-component />
+    <hr />
+    <usuario-component />
   </div>
 </template>
 
 <script>
-
-import HomeComponent from './components/HomeComponent.vue';
+import HomeComponent from "./components/HomeComponent.vue";
+import UsuarioComponent from "./components/UsuarioComponent.vue";
+import EstiloComponent from "./components/EstiloComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+  created() {
+    console.log("created");
   },
 };
 </script>
