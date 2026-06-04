fe915597d2e80d32e18dd2ff5fbf145a10c3e5b1
Maria Clara Bertin Vieira Ferreira <bertinmaria@dinfor.uniceub>
2026-06-03 21:56:49 -0300

    pedidos

--- MariaClaraBertin/ProjetoFinal/tburguer/src/views/PedidosView.vue
+++ MariaClaraBertin/ProjetoFinal/tburguer/src/views/PedidosView.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>Olá Pedidos</h1>
+    <h1>Pedidos</h1>
+    <ListaPedidoComponent/>
   </div>
 </template>
 <script>
+import ListaPedidoComponent from '@/components/ListaPedidoComponent.vue';
+
 export default {
   name: "PedidosView",
+  components: {
+    ListaPedidoComponent,
+  },
 };
 </script>
 <style scoped></style>
