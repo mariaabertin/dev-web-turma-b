92315518fa59264624c217ddd0f4a88a5364814d
Fabio Ruan <fabioruanmoreiradealencar@gmail.com>
2026-06-26 00:51:53 -0300

    chore: Finalize GitHub Pages configuration with correct publicPath

--- Fábio Ruan/Projeto Final/petspa/vue.config.js
+++ Fábio Ruan/Projeto Final/petspa/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dev-web-turma-b/Fábio Ruan/Projeto Final/petspa/'
+    ? '/dev-web-turma-b/'
     : '/'
 })
