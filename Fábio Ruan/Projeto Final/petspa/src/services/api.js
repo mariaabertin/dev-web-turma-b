1334bbc641379346e5434613f0166449134e82c1
Fabio Ruan <fabioruanmoreiradealencar@gmail.com>
2026-06-25 23:16:46 -0300

    Fix: Atualiza a BASE_URL da API para producao

--- Fábio Ruan/Projeto Final/petspa/src/services/api.js
+++ Fábio Ruan/Projeto Final/petspa/src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://my-json-server.typicode.com/ofxzinho/dev-web-turma-b";
 
 async function tratarResposta(resposta) {
   if (!resposta.ok) {
